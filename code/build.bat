89accaafe1e3a5d0066d49c7657910c4b0d2cc11
Josh <jtdonahue360@gmail.com>
2018-11-17 06:55:50 -0500

    fix typo in build script

--- code/build.bat
+++ code/build.bat
@@ -1,8 +1,8 @@
 @echo off
 
 
-set LIB_DIR=C:\Users\projects\Random_Projects\SDLProject\SDL\lib\x64
-set INCLUDE_DIR=C:\Users\projects\Random_Projects\SDLProject\SDL\include
+set LIB_DIR=C:\Users\JoshPC\projects\Random_Projects\SDLProject\SDL\lib\x64
+set INCLUDE_DIR=C:\Users\JoshPC\projects\Random_Projects\SDLProject\SDL\include
 set LINKER_OPTIONS=/LIBPATH:%LIB_DIR%
 
 
